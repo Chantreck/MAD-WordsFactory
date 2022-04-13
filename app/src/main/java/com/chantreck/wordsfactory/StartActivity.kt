0d9fb65fb01b78b0e0684c32774af1c30f48c4f4
Chantreck <vladnetaev@mail.ru>
2022-04-13 16:58:19 +0700

    Added ViewPager and page layout

--- app/src/main/java/com/chantreck/wordsfactory/StartActivity.kt
+++ app/src/main/java/com/chantreck/wordsfactory/StartActivity.kt
@@ -2,12 +2,24 @@ package com.chantreck.wordsfactory
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.chantreck.wordsfactory.databinding.ActivityStartBinding
+import com.chantreck.wordsfactory.onboarding.OnboardingAdapter
+import com.chantreck.wordsfactory.onboarding.OnboardingData
 
 class StartActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityStartBinding.inflate(layoutInflater) }
+    private val adapter = OnboardingAdapter()
+    private val pages = listOf(
+        OnboardingData(1, R.string.onboarding_title1, R.drawable.img_onboard1, R.string.next),
+        OnboardingData(2, R.string.onboarding_title2, R.drawable.img_onboard2, R.string.next),
+        OnboardingData(3, R.string.onboarding_title3, R.drawable.img_onboard3, R.string.lets_start)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
 
-
+        binding.onboardingViewPager.adapter = adapter
+        adapter.submitList(pages)
     }
 }
